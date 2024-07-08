9676439ed971605207d44db31c4c7123afbf7736
Minnah Basit <basit@usc.edu>
2024-07-08 13:48:38 -0700

    intializing react project

---
+++ Swift:SwiftUI Projects/Practice/Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Mia Basit on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
